5656dd73d602ddd76bc2222ad05be7f699819657
Hilder Arrieta <hbarrieta25@gmail.com>
2021-08-22 10:00:49 -0500

    Backend set

---
+++ BackEnd/test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DemoApplication
+{
+ class Program
+  { 
+   static void Main(string[] args) 
+   {
+    Boolean status=true;
+    Console.Write(status);
+    
+    Console.ReadKey();
+   }
+  }
+}
