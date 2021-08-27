49b3a17fa8f87539733c475205c5cb01b733317d
Hilder Arrieta <38758814+matlab98@users.noreply.github.com>
2021-08-27 15:07:07 -0500

    BackEnd

---
+++ back/Services/getDataService.cs
@@ -0,0 +1,37 @@
+﻿using back.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back.Services
+{
+    public class getDataService : IDataService
+    {
+        public getData_Result Get(string ciudad)
+        {
+            getData_Result _oData = new getData_Result();
+            using (IDbConnection con = new SqlConnection(Global.connectionString))
+            {
+                if (con.State == ConnectionState.Closed) con.Open();
+
+                var oHistory = con.Query<getData_Result>("getDataWeather", this.SetParameters("New York")).ToList();
+
+                if (oHistory != null && oHistory.Count() > 0)
+                {
+                    _oData = oHistory.SingleOrDefault();
+                }
+            }
+            return _oData;
+        }
+
+        private DynamicParameters SetParameters(string data) {
+
+            DynamicParameters parameters = new DynamicParameters();
+            return parameters;
+        }
+    }
+}
